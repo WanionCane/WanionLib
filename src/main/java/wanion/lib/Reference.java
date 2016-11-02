0849bb27c2446bd2c2ac832c63ce5c99f1e0e66b
WanionCane <wanioncane@gmail.com>
2016-11-02 13:37:24 -0200

    Updated to 1.2

--- src/main/java/wanion/lib/Reference.java
+++ src/main/java/wanion/lib/Reference.java
@@ -12,7 +12,7 @@ public final class Reference
 {
 	public static final String MOD_ID = "wanionlib";
 	public static final String MOD_NAME = "WanionLib";
-	public static final String VERSION = "1.7.10-1.1";
+	public static final String VERSION = "1.7.10-1.2";
 	public static final String ACCEPTED_MINECRAFT = "[1.7.10]";
 
 	private Reference() {}
--- src/main/java/wanion/lib/common/MetaItem.java
+++ src/main/java/wanion/lib/common/MetaItem.java
@@ -12,14 +12,17 @@ import cpw.mods.fml.common.registry.FMLControlledNamespacedRegistry;
 import cpw.mods.fml.common.registry.GameData;
 import gnu.trove.list.TIntList;
 import gnu.trove.list.array.TIntArrayList;
+import gnu.trove.map.TIntIntMap;
 import gnu.trove.map.TIntLongMap;
 import gnu.trove.map.TIntObjectMap;
+import gnu.trove.map.hash.TIntIntHashMap;
 import gnu.trove.set.TIntSet;
 import gnu.trove.set.hash.TIntHashSet;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
 
+import javax.annotation.Nonnull;
 import java.util.Collection;
 
 @SuppressWarnings("unused")
@@ -81,5 +84,36 @@ public final class MetaItem
 	{
 		for (final int id : getArray(itemStackCollection))
 			map.put(id, defaultValue);
+	}
+
+	public static TIntIntMap getKeySizeMap(final int startIndex, final int endIndex, @Nonnull ItemStack[] itemStacks)
+	{
+		final TIntIntMap keySizeMap = new TIntIntHashMap();
+		for (int i = startIndex; i < endIndex; i++) {
+			if (itemStacks[i] == null)
+				continue;
+			final int key = get(itemStacks[i]);
+			if (keySizeMap.containsKey(key))
+				keySizeMap.put(key, keySizeMap.get(key) + 1);
+			else
+				keySizeMap.put(key, 1);
+		}
+		return keySizeMap;
+	}
+
+	public static TIntIntMap getSmartKeySizeMap(final int startIndex, final int endIndex, @Nonnull ItemStack[] itemStacks)
+	{
+		final TIntIntMap smartKeySizeMap = new TIntIntHashMap();
+		for (int i = startIndex; i < endIndex; i++) {
+			final ItemStack itemStack = itemStacks[i];
+			if (itemStack == null)
+				continue;
+			final int key = get(itemStack);
+			if (smartKeySizeMap.containsKey(key))
+				smartKeySizeMap.put(key, smartKeySizeMap.get(key) + itemStack.stackSize);
+			else
+				smartKeySizeMap.put(key, itemStack.stackSize);
+		}
+		return smartKeySizeMap;
 	}
 }
